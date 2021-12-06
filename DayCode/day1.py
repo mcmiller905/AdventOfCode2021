becd11306fa31af20da6671862c931e2ea2e04a9
Mike <mcmiller905@gmail.com>
2021-12-06 09:42:42 -0500

    finished day 1

--- DayCode/day1.py
+++ DayCode/day1.py
@@ -2,11 +2,14 @@ def runCode():
     inputFile = open('InputFiles/day1Input.txt', 'r')
     numbers = inputFile.readlines()
 
-    currentDepth = int(numbers[0])
+    currentDepth = int(numbers[0]) + int(numbers[1]) + int(numbers[2])
     dropCounter = 0
-    for depth in numbers :
-        int_depth = int(depth)
+    line = 0
+    max = len(numbers) - 2
+    while line < max :
+        int_depth = int(numbers[line]) + int(numbers[line + 1]) + int(numbers[line+ 2])
         if int_depth > currentDepth:
             dropCounter = dropCounter + 1
         currentDepth = int_depth
+        line = line + 1
     print("Depth increases: " + str(dropCounter))
